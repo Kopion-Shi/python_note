892cc51781ced856f113c8d25d898e3fa866a200
Kopion-Shi <carl3633524461@outlook.com>
2023-05-08 18:37:16 +0800

    Add d2l

---
+++ base_study/process_study.py
@@ -0,0 +1,18 @@
+import multiprocessing
+import time
+import threading
+import multiprocessing
+
+class MyThread(threading.Thread):
+
+    def run(self):
+        time.sleep(1)
+        a = 1 + 1
+        print(a)
+
+    def test_2(self):
+        time.sleep(1)
+        b = 1 + 2
+        print(b)
+
+if __name__=='__main__':
